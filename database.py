5c6e3f09d67493f18b4612eea1c05f813e94b021
mrroboknow <mrroboknow@gmail.com>
2025-08-07 14:01:45 +0200

    pop

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # !!! WAŻNE: PONIŻEJ WKLEJ SWÓJ ADRES URL Z RENDER.COM !!!
-SQLALCHEMY_DATABASE_URL = "postgres://uzytkownik:haslo@dpg-adres-serwera/nazwa_bazy"
+SQLALCHEMY_DATABASE_URL = "postgresql://warsztat_user:xKoCBmPizi6sVsSm8T7VTDKylWl42KhT@dpg-d2a6jpbuibrs73bumu00-a/warsztat"
 
 # Tworzymy "silnik" bazy danych
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
